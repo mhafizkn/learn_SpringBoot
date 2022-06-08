79c4f4d88fd6716e01f822450cec194bf4987420
mhafizkn <hafizkn98@gmail.com>
2022-06-08 16:41:25 +0800

    Created model :tada:

--- src/main/java/com/example/learn/LearnApplication.java
+++ src/main/java/com/example/learn/LearnApplication.java
@@ -1,10 +1,13 @@
 package com.example.learn;
 
+import com.example.learn.Model.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -19,5 +22,12 @@ public class LearnApplication {
 	@GetMapping("/")
 	public List<String> hello() {
 		return List.of("Spring Boot - Main Page", "Hello World");
+	}
+
+	@GetMapping("/student")
+	public List<Student> students() {
+		return List.of(
+				new Student(1L, "Hafiz", "hafizxus@gmail.com", LocalDate.of(1998, Month.AUGUST, 26), 24)
+		);
 	}
 }
