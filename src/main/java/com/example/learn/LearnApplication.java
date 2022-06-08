cf3ccea47410797dbf21134a6ff4a2c099fa12ac
mhafizkn <hafizkn98@gmail.com>
2022-06-08 16:28:54 +0800

    Simple API :zzz:

--- src/main/java/com/example/learn/LearnApplication.java
+++ src/main/java/com/example/learn/LearnApplication.java
@@ -2,12 +2,22 @@ package com.example.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class LearnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnApplication.class, args);
 	}
 
+	// Return as JSON list
+	@GetMapping("/")
+	public List<String> hello() {
+		return List.of("Spring Boot - Main Page", "Hello World");
+	}
 }
